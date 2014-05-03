e5f429dd0bcaf3bead9614dd3bb5006fa87f50e5
Alex Fedosin <nsdfxela@gmail.com>
2014-05-03 23:02:47 +0400

    some reading and transform operations

--- a4ex1.R
+++ a4ex1.R
@@ -2,7 +2,12 @@
 
 best <- function(state, outcome) {
   ## Read outcome data
+  outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  neededColumns <- grep("^Hospital.30.Day.Readmission.Rates.from", names(outcome))
+  outcome[,neededColumns] <- as.numeric (outcome[,neededColumns])
+  lapply(outcome[,neededColumns], function(x) as.numeric(x))
   ## Check that state and outcome are valid
   ## Return hospital name in that state with lowest 30-day death
   ## rate
 }
+
